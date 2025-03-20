7ac075e127389d3e449cc687c8801b810a7b79d3
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2025-03-20 21:33:24 +0100

    feat: add data to the Model container and convey them to the template

--- src/main/java/com/springboot/thymeleaf/controller/HelloNameController.java
+++ src/main/java/com/springboot/thymeleaf/controller/HelloNameController.java
@@ -1,6 +1,8 @@
 package com.springboot.thymeleaf.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,9 +14,18 @@ public class HelloNameController {
     }
 
     // controller method to process HTML form
-
     @RequestMapping("/process-form")
-    public String helloName(){
+    // HttpServletRequest request - holds HTML form data
+    // Container for the data added by programmer
+    public String helloName(HttpServletRequest request, Model model){
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("userName");
+
+        // convert the data to all caps
+        String result = theName.toUpperCase();
+
+        // add message to the model
+        model.addAttribute("message", result);
         return "hello-name";
     }
 
