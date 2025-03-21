1cc68250e5461a73fa0b89d88d7131e981811623
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2025-03-21 16:25:46 +0100

    refactor: switch from GET to POST mapping in HelloNameController

--- src/main/java/com/springboot/thymeleaf/controller/HelloNameController.java
+++ src/main/java/com/springboot/thymeleaf/controller/HelloNameController.java
@@ -2,19 +2,28 @@ package com.springboot.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloNameController {
     // controller method to show initial HTML form
-    @RequestMapping("/show-form")
+    @GetMapping("/show-form")
     public String showForm(){
         return "input-name";
     }
 
     // controller method to process HTML form
-    @RequestMapping("/process-form")
+
+    // GET: The userName is visible in the URL
+    // User -> [Form: input-name.html] -> [URL with Query String] ->
+    // [Controller: helloName()] -> [View: hello-name.html]
+
+    // POST: The userName is hidden inside the request body
+    // User -> [Form: input-name.html] -> [HTTP Body] ->
+    // [Controller: helloName()] -> [View: hello-name.html]
+    @PostMapping("/process-form")
     // Assign theName to value of request parameter - userName
     public String helloName(@RequestParam("userName") String theName, Model model){
         // convert the data to all caps
