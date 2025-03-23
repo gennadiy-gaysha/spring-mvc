b7d0cf49a76eac3e06d33c37401c2913b2561d54
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2025-03-23 10:44:31 +0100

    feat: retirieve list of countries for dropdown from application.properties

--- src/main/java/com/springboot/thymeleaf/controller/UserController.java
+++ src/main/java/com/springboot/thymeleaf/controller/UserController.java
@@ -1,17 +1,25 @@
 package com.springboot.thymeleaf.controller;
 
 import com.springboot.thymeleaf.model.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class UserController {
+    @Value("${countries}")
+    List<String> countries;
+
+
     @GetMapping("/user-form")
     public String showUserForm(Model model){
         model.addAttribute("user", new User());
+        model.addAttribute("countries", countries);
         return "user-form";
     }
 
